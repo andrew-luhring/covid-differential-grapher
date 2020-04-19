e261dc6fb3bbcb5c54060be36fdc26ce9ce32722
Andrew Luhring <andrew.luhring@gmail.com>
2020-04-19 00:12:51 -0500

    removes excess

--- bin/__init__.py
+++ bin/__init__.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
-import json
-import jsonpickle
-from helpers import Helpers
-from fips import Fips
 from statistics import Statistics
-from master_obj import MasterObj
 from populate import populate
 
 populated = populate()
-stats = Statistics(populated)
 days_since_start = len(populated.listDates())
+stats = Statistics(populated)
+
 
 for item in stats.cases:
     print(item)
