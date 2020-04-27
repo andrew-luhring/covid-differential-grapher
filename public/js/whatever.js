3a2607fb431fbe62028b6130c39942c0afebace7
Andrew Luhring <andrew.luhring@gmail.com>
2020-04-26 22:29:45 -0500

    creates a functioning chart

--- public/js/whatever.js
+++ public/js/whatever.js
@@ -1,18 +1,59 @@
+function findHighest(data){
+  let highest = 0;
+
+  for(let [statename, state] of Object.entries(data)){
+    let len = state.death_differentials.length;
+    if(len > highest){
+      highest = len;
+    }
+  }
+  return highest;
+}
 
 
-function createMasterObj(data){
-  console.log(data)
+function drawStuff(jsondata){
+  let data = [];
+  let highest = findHighest(jsondata.states);
 
+  for(let [statename, state] of Object.entries(jsondata.states)){
+    let dds = state.death_differentials;
+    if(state.death_differentials.length < highest){
+      let zeroes = new Array(highest - state.death_differentials.length);
+      zeroes.fill(0)
+      dds.unshift(zeroes)
+      dds = dds.flat()
+    }
+    let color = `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 1)`
+    let obj = {label: statename, data: dds, borderColor: color, backgroundColor: 'rgb(255,255,255,0.0)', hidden: true}
+    data.push(obj)
+  }
+// Math.floor(Math.random() * 255)
 
-    // mo[fips.state][key] = fips
-  
-  //
-  // for(let [statename, state] of Object.entries(mo)){
-  //   for(let [fipsName, fips] of Object.entries(state)){
-  //
-  //
-  //   }
-  // }
+  var ctx = document.getElementById('myChart').getContext('2d');
+  let arr = new Array(99)
+  arr.fill(0)
+  var chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels:arr,
+          datasets: data
+      },
+      options: {
+        legend: {
+          display: true,
+        },
+        scales: {
+            yAxes: [{
+                stacked: true
+            }],
+            xAxes: [{
+                stacked: true
+            }]
+        }
+
+      }
+  });
+  console.log(chart)
 }
 
 
@@ -23,13 +64,10 @@ function createMasterObj(data){
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
         jsondata = JSON.parse(httpRequest.responseText)
-
-        createMasterObj(jsondata)
+        drawStuff(jsondata)
       }
     }
   }
   httpRequest.open('GET', './js/data.json');
   httpRequest.send();
 }())
-
-var ctx = document.getElementById('myChart').getContext('2d');
